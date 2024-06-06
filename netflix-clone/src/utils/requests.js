c9cfba6c903783da4ac5ea61b93b833dfea32afd
Firdos <mohammedfirdos9980@gmail.com>
2024-06-06 20:54:16 +0300

    banner

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -2,10 +2,11 @@ const API_KEY=process.env.REACT_APP_API_KEY;
 
 const requests ={
     fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchAdventureMovies:`/discover/movie?api_key=${API_KEY}&with_genres=12`,
     fetchAnimationMovies:`/discover/movie?api_key=${API_KEY}&with_genres=16`,
     fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchDocumentaryMovies:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
 
    
 }
